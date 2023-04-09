7897d59abcecdfec088a3a4170b9ee841910ed06
Alexandr <hamlack81@gmail.com>
2023-04-09 10:43:42 +0300

    docs: add some code

--- dz_1/Program.cs
+++ dz_1/Program.cs
@@ -25,24 +25,24 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-Console.WriteLine("Введите число 1 ");
-int number_1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число 2 ");
-int number_2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число 3 ");
-int number_3 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 1 ");
+// int number_1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 2 ");
+// int number_2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 3 ");
+// int number_3 = Convert.ToInt32(Console.ReadLine());
 
-int max = number_1;
+// int max = number_1;
 
-if(number_2 > max)
-{   
-    max = number_2;
-}
-if(number_3 > max)
-{
-    max = number_3;
-}
-Console.Write("число " + max + " является максимальным числом");
+// if(number_2 > max)
+// {   
+//     max = number_2;
+// }
+// if(number_3 > max)
+// {
+//     max = number_3;
+// }
+// Console.Write("число " + max + " является максимальным числом");
 
 
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
@@ -66,3 +66,22 @@ Console.Write("число " + max + " является максимальным 
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int result = 0;
+
+while(count < number-1)
+{
+        if(number % 2 == 0)
+            {
+                result = result + 2;
+                Console.WriteLine(result);
+            }
+        else
+             {
+                Console.WriteLine("не является четным числом");
+            }
+    count = count + 1;
+}
