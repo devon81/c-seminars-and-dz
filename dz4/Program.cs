a71e2a893c1fe5a4dd45c37455e2d4915fa64348
Alexandr <hamlack81@gmail.com>
2023-04-26 20:25:15 +0300

    feat: create foulder dz4

---
+++ dz4/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число A:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int numbers()
+{
+    int count = 0;
+    int pow = 1;
+
+    for( count = 0; count < number2; count++)
+    {
+        pow = pow*number1;
+    }
+    return pow;
+}
+Console.WriteLine(numbers());
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberN)
+  {
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array2 = new int[8];
+
+for(int i=0;i<array2.Length;i++)
+{
+    array2[i] = new Random().Next(0,1000);
+    Console.Write(array2[i]+",");
+}
